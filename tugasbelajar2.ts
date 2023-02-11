d01290b44459f52299994c3cbb091f5b9be21509
Dimaz <dimaz.al.fatihah.hilmi@gmail.com>
2023-02-11 11:25:50 +0700

    nyoba

---
+++ tugasbelajar2.ts
@@ -0,0 +1,95 @@
+//  Nomor SATU  (1) /////////////////////////////////////////////////////////
+
+// for (let y = 0; y <= 10; y++) {
+//   for (let x = 0; x <= 10; x++) {
+//     if (y >= x) {
+//       document.write(" * ");
+//     } else {
+//       document.write(" _ ");
+//     }
+//   }
+//   document.write("<br>");
+// }
+
+// document.write("<br>");
+
+//  Nomor : DUA (2) /////////////////////////////////////////////////////////
+
+// for (let y = 0; y <= 10; y++) {
+//   for (let x = 0; x <= 10; x++) {
+//     if (y <= x - 1 || y + x <= 9) {
+//       document.write(" _ ");
+//     } else {
+//       document.write(" * ");
+//     }
+//   }
+//   document.write("<br>");
+// }
+
+// document.write("<br>");
+
+// Nomor TIGA (3) ////////////////////////////////////////////////////////////
+
+// for (let y = 0; y <= 10; y++) {
+//   for (let x = 0; x <= 10; x++) {
+//     if (y <= x - 1 || y + x <= 9 || y < 9) {
+//       document.write(" _ ");
+//     } else {
+//       document.write(" * ");
+//     }
+//   }
+//   document.write("<br>");
+// }
+
+// document.write("<br>");
+
+//  Nomor EMPAT (4) //////////////////////////////////////////////////////////
+
+// let hewan: any = ["lion", "elephant", "squirrel ", "eagle", "zebra", "tiger"];
+
+// hewan.shift();
+// hewan.pop();
+
+// document.write(hewan);
+
+//  Nomor LIMA (5)  ////////////////////////////////////////////////////////////
+
+// let santri: any = [
+//   " Yusuf",
+//   "Fatih",
+//   "Fajar",
+//   "El Ghifari",
+//   "Faqih",
+//   "Ramdhan",
+//   "David",
+//   "Juhadi",
+//   "A Faqih",
+//   "Hilmi",
+//   "Rafi",
+// ];
+
+// document.write(santri.sort());
+
+//  Nomor ENAM (6) //////////////////////////////////////////////////////////////
+
+// let mobile: any = ["A Faqih", "David", "El Ghifari", "Fajar", "Faqih"];
+
+// let backend: any = ["Arga", "Ramdhan", "Luqman", "Andaru", "Aldimas"];
+
+// let gabung = mobile.concat(backend);
+
+// document.write(gabung);
+
+//  Nomor TUJUH (7) /////////////////////////////////////////////////////////////
+
+const fruits: any = ["Banana", "Orange", "Apple", "Mango"];
+fruits.join();
+console.log(fruits);
+
+// Nomor DELAPAN (8) ////////////////////////////////////////////////////////////
+
+// let animals: any = ["lion", "elephant", "squirrel ", "eagle", "zebra", "tiger"];
+
+// animals.splice(2, 3);
+
+// document.write(animals);
